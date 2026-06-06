9e693311394654b696d3b10a336f9c98ab990fc8
Bassem <Bassem.m.sayed@outlook.com>
2026-06-06 20:54:37 +0300

    starting work on analyses

--- models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
+++ models/staging/jaffle_shop/stg_jaffle_shop__orders.sql
@@ -1,6 +1,17 @@
-select
-    id as order_id,
-    user_id as customer_id,
-    order_date,
-    status
-from {{ source('jaffle_shop', 'orders') }}
+with raw as(
+    
+    select * from {{ source('jaffle_shop', 'orders') }}
+
+),
+renamed as (
+
+    select
+        id as order_id,
+        user_id,
+        order_date,
+        status as order_status
+    from raw
+
+)
+
+select * from renamed
